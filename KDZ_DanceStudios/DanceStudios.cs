d4c24cf8ff62b223439aada6bf091d506cf3e7b0
Ася Цваева <aatsvaeva@edu.hse.ru>
2017-03-14 21:14:27 +0300

    Added a new class, made a menu with items on the window

---
+++ KDZ_DanceStudios/DanceStudios.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDZ_DanceStudios
+{
+    class DanceStudios
+    {
+            private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string _metro;
+
+        public string Metro
+        {
+            get { return _metro; }
+            set { _metro = value; }
+        }
+        private int _price;
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        private double _rating;
+
+        public double Rating
+        {
+            get { return _rating; }
+            set { _rating = value; }
+        }
+
+        public DanceStudios(string name, string metro, int price, double rating)
+        {
+            _name = name;
+            _metro = metro;
+            _price = price;
+            _rating = rating;
+        }
+
+    }
+}
